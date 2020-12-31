521e828e51a6c477587d12e0d8403b6367556531
MorenaNobre <morenagnobre@gmail.com>
2020-12-31 12:35:33 -0300

    React-Project-Real-State-Design

---
+++ src/data/SliderData.js
@@ -0,0 +1,69 @@
+import ImageOne from '../images/houseOne.jpg'
+import ImageTwo from '../images/houseTwo.jpg'
+import ImageThree from '../images/houseThree.jpg'
+import ImageFour from '../images/houseFour.jpg'
+// import ImageFive from '../images/houseFive.jpg'
+// import ImageSix from '../images/houseSix.jpg'
+// import ImageSeven from '../images/houseSeven.jpg'
+
+export const SliderData = [
+  {
+    title: 'Luxury Villa in Bali, Indonesia',
+    price: '$4,280,000',
+    path: '/homes',
+    label: 'View Home',
+    image: ImageOne,
+    alt: 'House'
+  },
+  {
+    title: 'House in San Jose, Costa Rica',
+    price: '$2,860,000',
+    path: '/homes',
+    label: 'View Home',
+    image: ImageTwo,
+    alt: 'House'
+  },
+  {
+    title: 'House in Calabasas, California',
+    price: '$8,420,000',
+    path: '/homes',
+    label: 'View Home',
+    image: ImageThree,
+    alt: 'House'
+  },
+  {
+    title: 'House in Houston, Texas',
+    price: '$6,660,000',
+    path: '/homes',
+    label: 'View Home',
+    image: ImageFour,
+    alt: 'House'
+  }
+]
+
+// export const SliderDataTwo = [
+//   {
+//     title: 'House in Las Vegas, Indonesia',
+//     price: '$8,980,000',
+//     path: '/homes',
+//     label: 'View Home',
+//     image: ImageFive,
+//     alt: 'House'
+//   },
+//   {
+//     title: 'House in Houston, Texas',
+//     price: '$3,460,000',
+//     path: '/homes',
+//     label: 'View Home',
+//     image: ImageSix,
+//     alt: 'House'
+//   },
+//   {
+//     title: 'House in Alajuela, Costa Rica',
+//     price: '$5,420,000',
+//     path: '/homes',
+//     label: 'View Home',
+//     image: ImageSeven,
+//     alt: 'House'
+//   }
+// ]
