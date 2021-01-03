8e06d45c4d04d76f10ddb43213a09731addc2525
MorenaNobre <morenagnobre@gmail.com>
2021-01-03 13:59:34 -0300

    real-state-react-page-done

---
+++ src/data/InfoData.js
@@ -0,0 +1,22 @@
+import ImageOne from '../images/houseEight.jpg'
+import ImageTwo from '../images/houseNine.jpg'
+
+export const InfoData = {
+  heading: 'Explore our beautiful homes',
+  paragraphOne: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque laudantium, doloribus debitis quisquam perspiciatis tenetur praesentium suscipit accusantium enim saepe!',
+  paragraphTwo: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit neque itaque iste porro facilis veniam.',
+  buttonLabel: 'View Homes',
+  image: ImageOne,
+  reverse: false,
+  delay: 100
+};
+
+export const InfoDataTwo = {
+  heading: 'Modern Designs',
+  paragraphOne: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque laudantium',
+  paragraphTwo: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+  buttonLabel: 'View Homes',
+  image: ImageTwo,
+  reverse: true,
+  delay: 300
+}
